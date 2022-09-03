c93968dba694a3432ae70a7fd9ad386e7e13cba9
Genivaldo Honorato <genivaldo.honorato@dcx.ufpb.br>
2022-09-03 16:43:54 -0300

    aula 01

--- controller/src/main/java/com/indracompany/treinamento/controller/rest/ClienteRest.java
+++ controller/src/main/java/com/indracompany/treinamento/controller/rest/ClienteRest.java
@@ -1,7 +1,13 @@
 package com.indracompany.treinamento.controller.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.indracompany.treinamento.model.entity.Cliente;
@@ -14,8 +20,10 @@ public class ClienteRest extends GenericCrudRest<Cliente, Long, ClienteService>{
 	@Autowired
 	private ClienteService clienteService;
 	
-
-
-	
+	@GetMapping(value = "/buscarPorCpf/{cpf}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody ResponseEntity <Cliente> buscaClientePorCpf(@PathVariable String cpf) {
+		Cliente cliente = clienteService.buscaClientePorCpf(cpf);
+		return new ResponseEntity<>(cliente, HttpStatus.OK);
+	}
 
 }
