957cc4ab4cb8346d206a914bdea65aca188473a4
jefferson <jeffersonricardoss10@gmail.com>
2022-09-06 01:01:18 -0300

    Busca por Nome

--- model/src/main/java/com/indracompany/treinamento/model/service/ClienteService.java
+++ model/src/main/java/com/indracompany/treinamento/model/service/ClienteService.java
@@ -26,7 +26,14 @@ public class ClienteService extends GenericCrudService<Cliente, Long, ClienteRep
 			throw new AplicacaoException(ExceptionValidacoes.ALERTA_NENHUM_REGISTRO_ENCONTRADO, cpf);
 		}
 		return cli;
-		
+	}
+	
+	public Cliente buscarClientePorNome (String nome) {
+		Cliente cli = clienteRepository.findByNome(nome);
+		if(cli == null) {
+			throw new AplicacaoException(ExceptionValidacoes.ALERTA_NENHUM_REGISTRO_ENCONTRADO, nome);
+		}
+		return cli;
 	}
 	  
 }
