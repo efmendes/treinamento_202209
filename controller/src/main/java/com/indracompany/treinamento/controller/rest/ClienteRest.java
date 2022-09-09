87b5395d5ab63505eed555976dec813d30a9e83f
treinamentoindra <treinamentoindra>
2022-09-09 13:00:13 -0300

    Atividade 1°

--- controller/src/main/java/com/indracompany/treinamento/controller/rest/ClienteRest.java
+++ controller/src/main/java/com/indracompany/treinamento/controller/rest/ClienteRest.java
@@ -1,7 +1,12 @@
 package com.indracompany.treinamento.controller.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.indracompany.treinamento.model.entity.Cliente;
@@ -10,12 +15,20 @@ import com.indracompany.treinamento.model.service.ClienteService;
 @RestController
 @RequestMapping("rest/clientes")
 public class ClienteRest extends GenericCrudRest<Cliente, Long, ClienteService>{
-	
-	@Autowired
-	private ClienteService clienteService;
-	
 
+    @Autowired
+    private ClienteService clienteService;
 
-	
+    @GetMapping(value = "buscarPorCpf/{cpf}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody ResponseEntity<Cliente> buscarCLientePorCpf(String cpf) {
+        Cliente cli = clienteService.buscarClientePorCpf(cpf);
+        return new ResponseEntity<>(cli, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "buscarPorNome/{nome}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody ResponseEntity<Cliente> buscarClientePorNome(String nome) {
+        Cliente cli = clienteService.buscarClientePorNome(nome);
+        return new ResponseEntity<>(cli, HttpStatus.OK);
+    }
 
 }
