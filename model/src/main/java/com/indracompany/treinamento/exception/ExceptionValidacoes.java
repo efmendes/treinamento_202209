36339c768f0ca66ea9adebeebb403ee7f95e990b
Leonardonps <leonardonpsilva@gmail.com>
2022-09-08 21:28:44 -0300

    adicionando a função de buscar nome

--- model/src/main/java/com/indracompany/treinamento/exception/ExceptionValidacoes.java
+++ model/src/main/java/com/indracompany/treinamento/exception/ExceptionValidacoes.java
@@ -28,12 +28,12 @@ public enum ExceptionValidacoes implements AplicacaoExceptionValidacoes {
   ERRO_CONTA_INVALIDA("msg.erro.conta.invalida", null, AplicacaoExceptionValidacoes.SEVERIDADE_ERRO),
   ERRO_SALDO_INEXISTENTE("msg.erro.saldo.inexistente", null, AplicacaoExceptionValidacoes.SEVERIDADE_ERRO),
   ERRO_CONTA_DUPLICADA("msg.erro.conta.duplicada", null, AplicacaoExceptionValidacoes.SEVERIDADE_ERRO),
-  
+  ERRO_NOME_COM_FORMATACAO_INVALIDA("msg.app.erro.nome.com.formatacao.invalida", null, AplicacaoExceptionValidacoes.SEVERIDADE_ERRO),
+
   
 
   // Mensagens Alterta
   ALERTA_NENHUM_REGISTRO_ENCONTRADO("msg.app.alerta.nenhum.registro.encontrado", null, AplicacaoExceptionValidacoes.SEVERIDADE_ALERTA),;
-
   @Getter
   @Setter
   private String codigoMsg;
--- model/src/main/java/com/indracompany/treinamento/model/service/ClienteService.java
+++ model/src/main/java/com/indracompany/treinamento/model/service/ClienteService.java
@@ -28,5 +28,46 @@ public class ClienteService extends GenericCrudService<Cliente, Long, ClienteRep
 		return cli;
 		
 	}
-	  
+		
+	public Cliente buscarClientePorNome(String nome){
+		
+		Cliente cli = clienteRepository.findByNome(nome.strip());
+		
+		boolean temNumeros = false;
+		boolean temCaracInvalidos = false;
+		
+		/* 
+		 * Para que esse FOR? - Foi usado para verificar se a
+		 * string pesquisada no lá no swagger tem número. 
+		 */
+		for(int i = 0; i < 10; i++) {
+			if(nome.contains(Integer.toString(i))){
+				temNumeros = true;
+			}
+		}
+	
+		/* 
+		 * Para que esse IF com esse matches esquisito? - Esse if foi usado para
+		 * ver se tem algum caracter indesejado na hora de pesquisar a string nome
+		 * (por exemplo: & $ # $). Esse conjunto de letras esquisito é uma regex
+		 * (expressão regular) que peguei na internet para validar se a formatação 
+		 * do nome é válida (aceita os acentos da língua portuguesa, mas não caracteres
+		 * impróprios para nomes).
+		 */
+		if(!nome.matches("[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$")){
+			temCaracInvalidos = true;
+		}
+		
+	
+		if(temNumeros || temCaracInvalidos){
+			throw new AplicacaoException(ExceptionValidacoes.ERRO_NOME_COM_FORMATACAO_INVALIDA, nome);
+		}
+		
+		if (cli == null) {
+			throw new AplicacaoException(ExceptionValidacoes.ALERTA_NENHUM_REGISTRO_ENCONTRADO, nome);
+		}
+		
+		return cli;
+	}
+	
 }
