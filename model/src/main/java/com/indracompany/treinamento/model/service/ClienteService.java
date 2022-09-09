fb5809a9b8d9819608886bc1fff6db17b6e0fc46
Victor Cavalcante <victormcavalcante@gmail.com>
2022-09-09 17:52:43 -0300

    Busca por nome implementada

--- model/src/main/java/com/indracompany/treinamento/model/service/ClienteService.java
+++ model/src/main/java/com/indracompany/treinamento/model/service/ClienteService.java
@@ -1,12 +1,47 @@
 package com.indracompany.treinamento.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.indracompany.treinamento.exception.AplicacaoException;
+import com.indracompany.treinamento.exception.ExceptionValidacoes;
 import com.indracompany.treinamento.model.entity.Cliente;
 import com.indracompany.treinamento.model.repository.ClienteRepository;
+import com.indracompany.treinamento.util.CpfUtil;
 
 @Service
 public class ClienteService extends GenericCrudService<Cliente, Long, ClienteRepository>{
 
-	  
+	@Autowired
+	private ClienteRepository clienteRepository;
+	public Cliente buscarClientePorCpf(String cpf) {
+		boolean cpfValido = CpfUtil.validaCPF(cpf);
+		
+		if (!cpfValido) {
+			throw new AplicacaoException(ExceptionValidacoes.ERRO_CPF_INVALIDO, cpf);
+		}
+		Cliente cli = clienteRepository.findByCpf(cpf);
+		if (cli == null) {
+			throw new AplicacaoException(ExceptionValidacoes.ALERTA_NENHUM_REGISTRO_ENCONTRADO, cpf);
+		}
+		return cli;
+		
+		
+	
+}
+	public Cliente buscarClientePorNome(String nome) {
+
+		if(nome.isEmpty()) {
+			throw new AplicacaoException(ExceptionValidacoes.ERRO_CAMPO_OBRIGATORIO, nome);
+		}
+
+		Cliente cli = clienteRepository.findByNome(nome);
+
+		if (cli == null) {
+			throw new AplicacaoException(ExceptionValidacoes.ALERTA_NENHUM_REGISTRO_ENCONTRADO, nome);
+		}
+
+		return cli;
+
+	}
 }
