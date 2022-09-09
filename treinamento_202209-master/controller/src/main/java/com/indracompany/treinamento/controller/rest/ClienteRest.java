390ea800c3f5232319f42cd4e5219929739a67ff
Arthur Formiga <85749009+Arthur1p@users.noreply.github.com>
2022-09-08 21:25:53 -0300

    Add files via upload

---
+++ treinamento_202209-master/controller/src/main/java/com/indracompany/treinamento/controller/rest/ClienteRest.java
@@ -0,0 +1,30 @@
+package com.indracompany.treinamento.controller.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.indracompany.treinamento.model.entity.Cliente;
+import com.indracompany.treinamento.model.service.ClienteService;
+
+@RestController
+@RequestMapping("rest/clientes")
+public class ClienteRest extends GenericCrudRest<Cliente, Long, ClienteService>{
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	@GetMapping(value = "/buscarPorCpf/{cpf}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody ResponseEntity<Cliente> buscarClientePorCpf(@PathVariable String cpf) {
+		Cliente cli = clienteService.buscarClientePorCpf(cpf);
+		return new ResponseEntity<>(cli, HttpStatus.OK);
+	}
+	
+
+}
