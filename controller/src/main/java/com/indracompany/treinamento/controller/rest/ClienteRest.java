38842e4e61a4f80d0960f05ff493c06deb909dff
DaviFarias7 <davic.farias7@gmail.com>
2022-09-09 20:25:59 -0300

    First commit

--- controller/src/main/java/com/indracompany/treinamento/controller/rest/ClienteRest.java
+++ controller/src/main/java/com/indracompany/treinamento/controller/rest/ClienteRest.java
@@ -1,7 +1,12 @@
 package com.indracompany.treinamento.controller.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.indracompany.treinamento.model.entity.Cliente;
@@ -14,8 +19,16 @@ public class ClienteRest extends GenericCrudRest<Cliente, Long, ClienteService>{
 	@Autowired
 	private ClienteService clienteService;
 	
-
-
+	@GetMapping(value = "buscarPorCpf/{cpf}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody ResponseEntity<Cliente> buscarCLientePorCpf(String cpf) {
+		Cliente cli = clienteService.buscarClientePorCpf(cpf);
+		return new ResponseEntity<>(cli, HttpStatus.OK);
+	}
 	
+	@GetMapping(value = "buscarPorNome/{nome}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody ResponseEntity<Cliente> buscarClientePorNome(String nome) {
+		Cliente cli = clienteService.buscarClientePorNome(nome);
+		return new ResponseEntity<>(cli, HttpStatus.OK);
+	}	
 
 }
--- model/src/main/java/com/indracompany/treinamento/model/repository/ClienteRepository.java
+++ model/src/main/java/com/indracompany/treinamento/model/repository/ClienteRepository.java
@@ -4,7 +4,8 @@ import com.indracompany.treinamento.model.entity.Cliente;
 
 public interface ClienteRepository extends GenericCrudRepository<Cliente, Long>{
 	
-
+	public Cliente findByCpf(String cpf);
 	
+	public Cliente findByNome(String nome);
 
 }
