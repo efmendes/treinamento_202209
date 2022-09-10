c7f930a33286b51ecb7a71d53f7ed62ee4553750
Igor Borges <igorborgeswrk2@gmail.com>
2022-09-10 16:30:48 -0300

    Correção de duplicidade do método em ClienteService.java

--- model/src/main/java/com/indracompany/treinamento/model/service/ClienteService.java
+++ model/src/main/java/com/indracompany/treinamento/model/service/ClienteService.java
@@ -39,18 +39,6 @@ public class ClienteService extends GenericCrudService<Cliente, Long, ClienteRep
 		
 	}
 
-	@Autowired
-	private ClienteRepository clientRepository;
-	
-	public Cliente buscarClientePorNome(String nome) {
-		
-		Cliente cli = clientRepository.findByNome(nome);
-		if (cli == null) {
-			throw new AplicacaoException(ExceptionValidacoes.ALERTA_NENHUM_REGISTRO_ENCONTRADO, nome);
-		}
-		return cli;
-		
-	}
 	  
 	public List<ClienteDTO> buscarClientePorNome(String nome) {
 		if (StringUtils.isBlank(nome) 
