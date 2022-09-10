9f2b7bd80212db56b677e3e9fabd932a133c27d7
RuanPontes <90006521+RuanPontes@users.noreply.github.com>
2022-09-09 22:21:07 -0300

    Update ClienteService.java

--- model/src/main/java/com/indracompany/treinamento/model/service/ClienteService.java
+++ model/src/main/java/com/indracompany/treinamento/model/service/ClienteService.java
@@ -28,5 +28,15 @@ public class ClienteService extends GenericCrudService<Cliente, Long, ClienteRep
 		return cli;
 		
 	}
+	
+	public Cliente buscarClientePorNome(String nome) {
+
+		Cliente cli = clienteRepository.findByNome(nome);
+		if (cli == null) {
+			throw new AplicacaoException(ExceptionValidacoes.ALERTA_NENHUM_REGISTRO_ENCONTRADO, nome);
+		}
+		return cli;
+
+	}
 	  
 }
