bcccd96745c4870bb03c8642c9e658240438addb
guilherme <dev.guilhermeribeiro@gmail.com>
2022-09-10 07:38:20 -0300

    buscar cliente por nome - ajustes

--- model/src/main/java/com/indracompany/treinamento/util/NomeUtil.java
+++ model/src/main/java/com/indracompany/treinamento/util/NomeUtil.java
@@ -3,7 +3,7 @@ package com.indracompany.treinamento.util;
 public class NomeUtil {
 
     public static boolean validaNome(String nome) {
-        String regexNome = "^(([A-za-z áéíóú ÁÉÍÓÚ âêô ÂÊÔ ã])*)$";
+        String regexNome = "^(([a-zA-z áéíóú ÁÉÍÓÚ âêô ÂÊÔ ã])*)$";
 
         if (!nome.matches(regexNome)) {
             return false;
