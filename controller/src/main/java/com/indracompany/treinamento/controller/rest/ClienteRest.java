00aaf51f9ef17fafa6e7ad3fde3301e63c436b2a
Edoardo Fama <edoardofama@hotmail.com>
2022-09-09 21:55:13 -0300

    busca cliente nome

--- controller/src/main/java/com/indracompany/treinamento/controller/rest/ClienteRest.java
+++ controller/src/main/java/com/indracompany/treinamento/controller/rest/ClienteRest.java
@@ -1,6 +1,8 @@
 package com.indracompany.treinamento.controller.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
