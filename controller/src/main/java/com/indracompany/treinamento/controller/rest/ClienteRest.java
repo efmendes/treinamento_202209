43247d906616ead1ca3d2018ead53170d68c8cd0
RianMS <riaanmsoares@gmail.com>
2022-09-12 21:12:55 -0300

    Commit

--- controller/src/main/java/com/indracompany/treinamento/controller/rest/ClienteRest.java
+++ controller/src/main/java/com/indracompany/treinamento/controller/rest/ClienteRest.java
@@ -1,5 +1,7 @@
 package com.indracompany.treinamento.controller.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.indracompany.treinamento.model.dto.ClienteDTO;
 import com.indracompany.treinamento.model.entity.Cliente;
 import com.indracompany.treinamento.model.service.ClienteService;
 
