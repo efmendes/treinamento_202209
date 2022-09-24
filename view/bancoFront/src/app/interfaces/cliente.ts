1f88abf53175872fe007c2cf64e7a753419c962d
AzevedoGabriel <gabriel.dantas.azevedo@ccc.ufcg.edu.br>
2022-09-24 07:48:04 -0300

    first commit front

--- view/bancoFront/src/app/interfaces/cliente.ts
+++
@@ -1,8 +0,0 @@
-export interface ICliente {
-  id: number;
-  nome: string;
-  cpf: string;
-  email: string;
-  ativo?: boolean;
-  observacoes?: string;
-}
--- view/bancoFront/src/app/interfaces/conta.ts
+++
@@ -1,8 +0,0 @@
-import { ICliente } from './cliente';
-export interface IConta {
-  id: number;
-  cliente: ICliente;
-  agencia: string;
-  numero: string;
-  saldo: number;
-}
